3ae589727f436964582bce53f065fd4b918c3656
faisal khan <faisal01khan2003@gmail.com>
2025-06-22 20:37:12 +0530

    Start Point

---
+++ src/main/java/com/example/mini_ecommerce/MiniEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.example.mini_ecommerce;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEcommerceApplication.class, args);
+	}
+
+}
