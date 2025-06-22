0e423dc48540a7e868719fe012273a519e4dc756
faisal khan <faisal01khan2003@gmail.com>
2025-06-22 20:48:51 +0530

    Added dotenv setup

--- src/main/java/com/example/mini_ecommerce/MiniEcommerceApplication.java
+++ src/main/java/com/example/mini_ecommerce/MiniEcommerceApplication.java
@@ -1,6 +1,7 @@
 package com.example.mini_ecommerce;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniEcommerceApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MiniEcommerceApplication.class, args);
 	}
 
